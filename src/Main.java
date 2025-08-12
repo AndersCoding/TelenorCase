ef2126faa313364bac1354ab8c878a9ffc98cd4c
AndersB <97296236+AndersCoding@users.noreply.github.com>
2025-08-12 13:34:01 +0200

    Added findNumbers-class, to find the highest number in the array, and it's location

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 //        }
         //GridNumbers.main(args);
         FindNumbers.findNumbers();
-        System.out.println("");
+        System.out.println(" ");
         FindNumbers.findHighestNumber();
 
     }
